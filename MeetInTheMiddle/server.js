64a4e0bfcd09b84288c79e93bacbe2af311215d8
Mateo Sanabria <37130201+Teo813@users.noreply.github.com>
2023-11-26 15:56:06 -0500

    Fixes to server.js for link handling

--- MeetInTheMiddle/server.js
+++ MeetInTheMiddle/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const addToCollection = require('./userAddEventsFunction');  
+const addToCollection = require('./userAddEventsFunction');
 const retrieveDocumentsByUserID = require('./userRetrieveEventsFunction.js');
 const handleCreateAccount = require('./registerAddEvent');
 const userValidation= require('./userValidation');
@@ -14,7 +14,9 @@ const delALLSavedEvents = require('./userDeleteAllSavedEvents.js');
 const app = express();
 const PORT = 3000;  // You can choose any port
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
-app.use(cors()); 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.get('/getNearbyPlaces', async (req, res) => {
     const { lat, lng, radius, types } = req.query;
     const endpoint = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${radius}&type=${types}&key=${GOOGLE_API_KEY}`;
@@ -26,9 +28,6 @@ app.get('/getNearbyPlaces', async (req, res) => {
         res.status(500).json({ error: "Failed to fetch data from Google Places API" });
     }
 });
-
-app.use(bodyParser.json());
-
 // Route for adding an event
 app.post('/addEvent', async (req, res) => {
     const { userID, eventName, address1, address2, meetingPoint } = req.body;
@@ -58,7 +57,7 @@ app.post('/checkEmail', async (req, res) => {
     try{
         console.log("Shaggy was here <3");
         const insertedId = await checkEmail(email);
-        
+
         res.json({ emailExists: insertedId});
     } catch (error) {
         res.status(500).json({ success: false, error: 'checking email didnt work 2. >:('});
@@ -68,7 +67,7 @@ app.post('/checkEmail', async (req, res) => {
 app.post('/validateUser', async (req, res) => {
     const {email, password} = req.body;
     console.log("Starting server try catch");
-    try{ 
+    try{
         console.log("Calling userValidation");
         const insertedId = await userValidation(email, password);
         console.log("InsertedID set");
@@ -78,7 +77,6 @@ app.post('/validateUser', async (req, res) => {
     }
 
 });
-
 // Route for saving a location
 app.post('/saveLocation', async (req, res) => {
     console.log('Received POST request to /saveLocation');
@@ -111,7 +109,7 @@ app.post('/retrieveEvent', async (req, res) => {
 app.post('/delALLEvents', async (req, res) => {
     const { userID } = req.body;
     console.log('test four', userID);
-    
+
     try {
         const deletedCount = await delALLSavedEvents(userID);
         res.json({ success: true, deletedCount }); // Corrected the property name to 'deletedCount'
@@ -123,7 +121,7 @@ app.post('/delALLEvents', async (req, res) => {
 app.post('/submit-address', async (req, res) => {
     const userID = req.body.userID;
     const address = req.body.address;
-
+        console.log(`ADDRESS SUMITTED with user ID: ${userID} and address ${address}`);
     // Set default values or modify these as per your requirement
     const eventName = "Default Event Name"; // Example default event name
     const address1 = address; // Assuming the submitted address is 'address1'
@@ -142,4 +140,4 @@ app.post('/submit-address', async (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-});
+});                                                             
